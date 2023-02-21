0a4693a9439ce7c7ff8ecf11c39267faf6dca09f
Yura <romanov.e180@gmail.com>
2023-02-21 19:54:32 +0300

    Первая задача

---
+++ Work1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+if (a*a==b)
+{
+Console.WriteLine("Второе число является квадратом первого");
+}
+else
+{
+    Console.WriteLine("Второе число не является квадратом первого")
+}
