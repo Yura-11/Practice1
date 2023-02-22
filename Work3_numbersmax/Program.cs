7867e83428ad3acb0afc0cb289631c35f3416462
Yura <romanov.e180@gmail.com>
2023-02-22 16:29:02 +0300

    3 задача

---
+++ Work3_numbersmax/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 1;
+int b = 15;
+int c = 6;
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+
+Console.Write("максимальное значение ");
+Console.WriteLine(max);
